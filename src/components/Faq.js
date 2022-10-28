6a1853677c6daa4d9fd6b32e9dceea7229e31a7a
RedEye75 <yusufbinemamstk@gmail.com>
2022-10-28 15:35:33 +0600

    some functionality added

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,137 +1,155 @@
-import React from 'react';
+import React from "react";
 
 const h1 = () => {
-	return (
-		<div>
-			<div className="grid space-y-4 bg-blue-100  lg:grid-cols-3 px-28 md:grid-cols-2 sm:grid-cols-1 gap-11">
-				<details class="group border-l-4 border-green-500 bg-gray-50 p-6" open>
-					<summary class="flex cursor-pointer items-center justify-between">
-						<h2 class="text-lg font-medium text-gray-900">1. What is online learning like?</h2>
+  return (
+    <div>
+      <div className="grid space-y-4   lg:grid-cols-3 px-28 md:grid-cols-2 sm:grid-cols-1 gap-11">
+        <details class="group border-l-4 border-green-500 bg-gray-50 p-6" open>
+          <summary class="flex cursor-pointer items-center justify-between">
+            <h2 class="text-lg font-medium text-gray-900">
+              1. What is online learning like?
+            </h2>
 
-						<span class="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-						</span>
-					</summary>
+            <span class="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+          </summary>
 
-					<p class="mt-4 leading-relaxed text-gray-700">
-						For those who have never encountered online learning, the biggest question is often what it is
-						actually like. The caveat, as you know, is that online learning comes in several forms.
-						Therefore, this is your chance to explain that to people. Point out that there are different
-						types of online courses and programs, and that they’ll have to decide which is best for them.
-						Then, explain the types of courses you offer and how your online programs work to give them all
-						the information they need. This not only gives them information, but it increases their trust
-						because you’re being transparent and giving them straightforward answers.
-					</p>
-				</details>
+          <p class="mt-4 leading-relaxed text-gray-700">
+            For those who have never encountered online learning, the biggest
+            question is often what it is actually like. The caveat, as you know,
+            is that online learning comes in several forms. Therefore, this is
+            your chance to explain that to people. Point out that there are
+            different types of online courses and programs, and that they’ll
+            have to decide which is best for them. Then, explain the types of
+            courses you offer and how your online programs work to give them all
+            the information they need. This not only gives them information, but
+            it increases their trust because you’re being transparent and giving
+            them straightforward answers.
+          </p>
+        </details>
 
-				<details class="group border-l-4 border-green-500 bg-gray-50 p-6">
-					<summary class="flex cursor-pointer items-center justify-between">
-						<h2 class="text-lg font-medium text-gray-900">2. Can I learn at my own pace?</h2>
+        <details class="group border-l-4 border-green-500 bg-gray-50 p-6">
+          <summary class="flex cursor-pointer items-center justify-between">
+            <h2 class="text-lg font-medium text-gray-900">
+              2. Can I learn at my own pace?
+            </h2>
 
-						<span class="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-						</span>
-					</summary>
+            <span class="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+          </summary>
 
-					<p class="mt-4 leading-relaxed text-gray-700">
-						This is another common question that people have about online courses. There are usually
-						self-paced programs and traditional classroom or meeting-style programs available. Today’s world
-						is full of busy people that want to better themselves, so the self-paced programs are popular.
-						Whether or not you provide this option, be sure to answer this question for people and explain
-						the pros and cons of each type of learning. As a bonus, you can even invite them to contact you
-						for more information or to discuss what their options are.
-					</p>
-				</details>
+          <p class="mt-4 leading-relaxed text-gray-700">
+            This is another common question that people have about online
+            courses. There are usually self-paced programs and traditional
+            classroom or meeting-style programs available. Today’s world is full
+            of busy people that want to better themselves, so the self-paced
+            programs are popular. Whether or not you provide this option, be
+            sure to answer this question for people and explain the pros and
+            cons of each type of learning. As a bonus, you can even invite them
+            to contact you for more information or to discuss what their options
+            are.
+          </p>
+        </details>
 
-				<details class="group border-l-4 border-green-500 bg-gray-50 p-6">
-					<summary class="flex cursor-pointer items-center justify-between">
-						<h2 class="text-lg font-medium text-gray-900">2. Do employers accept online degrees?</h2>
+        <details class="group border-l-4 border-green-500 bg-gray-50 p-6">
+          <summary class="flex cursor-pointer items-center justify-between">
+            <h2 class="text-lg font-medium text-gray-900">
+              2. Do employers accept online degrees?
+            </h2>
 
-						<span class="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-						</span>
-					</summary>
+            <span class="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+          </summary>
 
-					<p class="mt-4 leading-relaxed text-gray-700">
-						All new learning innovations are met with some degree of scrutiny, but skepticism subsides as
-						methods become more mainstream. Such is the case for online learning. Studies indicate employers
-						who are familiar with online degrees tend to view them more favorably, and more employers are
-						acquainted with them than ever before. The majority of colleges now offer online degrees,
-						including most public, not-for-profit, and Ivy League universities. Online learning is also
-						increasingly prevalent in the workplace as more companies invest in web-based employee training
-						and development programs.
-					</p>
-				</details>
+          <p class="mt-4 leading-relaxed text-gray-700">
+            All new learning innovations are met with some degree of scrutiny,
+            but skepticism subsides as methods become more mainstream. Such is
+            the case for online learning. Studies indicate employers who are
+            familiar with online degrees tend to view them more favorably, and
+            more employers are acquainted with them than ever before. The
+            majority of colleges now offer online degrees, including most
+            public, not-for-profit, and Ivy League universities. Online learning
+            is also increasingly prevalent in the workplace as more companies
+            invest in web-based employee training and development programs.
+          </p>
+        </details>
 
-				<details class="group border-l-4 border-green-500 bg-gray-50 p-6">
-					<summary class="flex cursor-pointer items-center justify-between">
-						<h2 class="text-lg font-medium text-gray-900">
-							2. How do I know if an online degree program is credible?
-						</h2>
+        <details class="group border-l-4 border-green-500 bg-gray-50 p-6">
+          <summary class="flex cursor-pointer items-center justify-between">
+            <h2 class="text-lg font-medium text-gray-900">
+              2. How do I know if an online degree program is credible?
+            </h2>
 
-						<span class="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-						</span>
-					</summary>
-					<p>
-						As with traditional colleges, some online schools are considered more credible than others.
-						Reputation, post-graduation employment statistics, and enrollment numbers are not always
-						reliable indicators of quality, which is why many experts advise students to look for accredited
-						schools. In order for an online college to be accredited, a third-party organization must review
-						its practices, finances, instructors, and other important criteria and certify that they meet
-						certain quality standards. The certifying organization matters, too, since accreditation is only
-						as reliable as the agency that grants it. Students should confirm online programs’ accrediting
-						agencies are recognized by the U.S. Department of Education and/or the Council on Higher
-						Education Accreditation before submitting their applications.
-					</p>
-				</details>
-			</div>
-		</div>
-	);
+            <span class="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+          </summary>
+          <p>
+            As with traditional colleges, some online schools are considered
+            more credible than others. Reputation, post-graduation employment
+            statistics, and enrollment numbers are not always reliable
+            indicators of quality, which is why many experts advise students to
+            look for accredited schools. In order for an online college to be
+            accredited, a third-party organization must review its practices,
+            finances, instructors, and other important criteria and certify that
+            they meet certain quality standards. The certifying organization
+            matters, too, since accreditation is only as reliable as the agency
+            that grants it. Students should confirm online programs’ accrediting
+            agencies are recognized by the U.S. Department of Education and/or
+            the Council on Higher Education Accreditation before submitting
+            their applications.
+          </p>
+        </details>
+      </div>
+    </div>
+  );
 };
 
 export default h1;
--- src/components/Register.js
+++ src/components/Register.js
@@ -18,38 +18,32 @@ const Register = () => {
   };
   return (
     <div>
-      <div className="w-full max-w-md p-8 space-y-3 rounded-xl dark:bg-gray-900 dark:text-gray-100">
+      <div className="w-full max-w-md p-8 space-y-3 ml-96 mt-40 dark:bg-gray-900 dark:text-gray-100">
         <h1 className="text-2xl font-bold text-center">Register</h1>
         <form
           novalidate=""
           action=""
-          className="space-y-6 ng-untouched ng-pristine ng-valid"
+          className="space-y-6 ng-untouched  ng-pristine ng-valid"
         >
           <div className="space-y-1 text-sm">
-            <label for="username" className="block dark:text-gray-400">
-              Username
-            </label>
             <input
               type="text"
               name="username"
               id="username"
               placeholder="Username"
-              className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
+              className="w-full px-4 py-4 rounded-md dark:border-gray-700 dark:bg-slate-300 dark:text-gray-100 focus:dark:border-violet-400"
             />
           </div>
           <div className="space-y-1 text-sm">
-            <label for="password" className="block dark:text-gray-400">
-              Password
-            </label>
             <input
               type="password"
               name="password"
               id="password"
               placeholder="Password"
-              className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
+              className="w-full px-4 py-4 bg-slate-300 rounded-md dark:border-gray-700 dark:text-gray-100 focus:dark:border-violet-400"
             />
           </div>
-          <button className="block w-full p-3 text-center rounded-sm dark:text-gray-900 dark:bg-violet-400">
+          <button className="block w-64 mx-auto p-3 text-center hover:bg-yellow-400 rounded-sm dark:text-gray-900 dark:bg-rose-800">
             Register
           </button>
         </form>
